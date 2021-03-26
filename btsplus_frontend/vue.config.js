eb00864917e0065ec783bca7d9d45436ed22bb4f
CappuccinoCup <CappuccinoCup@126.com>
2021-03-26 21:27:39 +0800

    version0.1 adjust some settings

---
+++ btsplus_frontend/vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+    host: 'localhost',
+    port: 80,
+    // 以下代码会使项目运行成功后自动打开浏览器
+    // open: true
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= 'BTSplus';
+        return args
+      })
+  },
+};
