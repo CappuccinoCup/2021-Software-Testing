5facbd1b2cb444a0f9cd1ec3e727e96206692320
YangYunteng <18302010022@fudan.edu.cn>
2021-04-11 22:36:00 +0800

    Part1 接口对接测试完毕

--- btsplus_frontend/src/components/teller/RepaymentAccounts.vue
+++ btsplus_frontend/src/components/teller/RepaymentAccounts.vue
@@ -47,50 +47,67 @@ export default {
         {text: '发放日期', align: 'center', value: 'loanDate'},
         {text: '操作', align: 'center', value: 'actions'}
       ],
-      accounts: [{
-        "id": 123,
-        "iouNum": "L2104032014071",
-        "creator": null,
-        "createdTime": "2021-04-03 20:14",
-        "loanDate": "2021-04-03",
-        "productCode": "20005",
-        "productName": "请键盘侠出门右转贷款",
-        "amount": 100000,
-        "interest": 667.3394,
-        "total": 100667.3394
-      },
-        {
-          "id": 321,
-          "iouNum": "L2104031952191",
-          "creator": null,
-          "createdTime": "2021-04-03 19:52",
-          "loanDate": "2021-04-03",
-          "productCode": "20001",
-          "productName": "买房贷款",
-          "amount": 20000,
-          "interest": 66.66,
-          "total": 20066.66
-        }],
-      customer: {
-        "id": 13,
-        "code": "AB2121202104031",
-        "name": "请不要侮辱二次元",
-        "creator": null,
-        "createdTime": "2021-04-03 19:33",
-        "sex": 0,
-        "phone": "13333333333",
-        "email": "3333333@fudan.edu.cn",
-        "address": "上海市杨浦区键盘侠只能出门右转2500号"
-      },
+      accounts: [],
+      customer: {},
+      // accounts: [{
+      //   "id": 123,
+      //   "iouNum": "L2104032014071",
+      //   "creator": null,
+      //   "createdTime": "2021-04-03 20:14",
+      //   "loanDate": "2021-04-03",
+      //   "productCode": "20005",
+      //   "productName": "请键盘侠出门右转贷款",
+      //   "amount": 100000,
+      //   "interest": 667.3394,
+      //   "total": 100667.3394
+      // },
+      //   {
+      //     "id": 321,
+      //     "iouNum": "L2104031952191",
+      //     "creator": null,
+      //     "createdTime": "2021-04-03 19:52",
+      //     "loanDate": "2021-04-03",
+      //     "productCode": "20001",
+      //     "productName": "买房贷款",
+      //     "amount": 20000,
+      //     "interest": 66.66,
+      //     "total": 20066.66
+      //   }],
+      // customer: {
+      //   "id": 13,
+      //   "code": "AB2121202104031",
+      //   "name": "请不要侮辱二次元",
+      //   "creator": null,
+      //   "createdTime": "2021-04-03 19:33",
+      //   "sex": 0,
+      //   "phone": "13333333333",
+      //   "email": "3333333@fudan.edu.cn",
+      //   "address": "上海市杨浦区键盘侠只能出门右转2500号"
+      // },
     }
   },
   methods: {
+
     viewBill: function (item) {
+      // let api = `/customer/loan/${this.iouNum}/bills`;
+      // this.axios.get(api,{
+      //   params:{}
+      // }).then(resp=>{
+      //   if(resp.data.code===200){
+      //     let data=resp.data.data;
+      //     if(data)
+      //   }
+      // })
       this.$router.push({
         path: '/teller/bill',
         name: 'Bill',
         query: {
-          iouNum: item.iouNum
+          iouNum: item.iouNum,
+          name: this.customer.name,
+          sex: this.customer.sex,
+          code: this.customer.code,
+          productName: this.productName,
+          productCode: this.productCode
         }
       })
     }
@@ -103,6 +120,7 @@ export default {
     }).then(resp => {
       if (resp.data.code === 200) {
         let data = resp.data.data;
+        console.log(data);
         if (data.length === 0) {
           this.app.message('目前没有贷款账户');
         } else {
@@ -110,16 +128,16 @@ export default {
           let accounts = [];
           for (let i = 0; i < data.length; i++) {
             let account = {
-              id: accounts[i].id,
-              iouNum: accounts[i].iouNum,
-              creator: accounts[i].creator,
-              createdTime: accounts[i].createdTime,
-              loanDate: accounts[i].loanDate,
-              productCode: accounts[i].productCode,
-              productName: accounts[i].productName,
-              amount: accounts[i].amount,
-              interest: accounts[i].interest,
-              total: accounts[i].total
+              id: data[i].id,
+              iouNum: data[i].iouNum,
+              creator: data[i].creator,
+              createdTime: data[i].createdTime,
+              loanDate: data[i].loanDate,
+              productCode: data[i].productCode,
+              productName: data[i].productName,
+              amount: data[i].amount,
+              interest: data[i].interest,
+              total: data[i].total
             };
             accounts.push(account)
           }
